44f843ef3d6116852918494da4f40a0d73852fb4
Elliott Biondo <biondoed@ornl.gov>
2024-10-07 09:53:20 -0400

    Date in caption

--- make.py
+++ make.py
@@ -14,7 +14,7 @@ for d in reversed(sorted(dirs)):
     pic_index = 0
     out +='        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">\n'
     desc = d[9:].replace("_", " ")
-    #out +='<h7>{}</h7>'.format(desc)
+    long_desc = "{}, {}".format(desc, d[:4])
     for p in sorted(pics):
         if p == ".DS_Store":
             continue
@@ -22,9 +22,9 @@ for d in reversed(sorted(dirs)):
         thumb_name = 'thumbs/{}_{}'.format(d, p)
 
         if pic_index == 0:
-            out +='            <a class="lightbox" href="{0}" data-caption="{2}"><img src="{1}" title="{2}"></a>\n'.format(name, thumb_name, desc)
+            out +='            <a class="lightbox" href="{0}" data-caption="{2}"><img src="{1}" title="{2}"></a>\n'.format(name, thumb_name, long_desc)
         else:
-            out +='            <a class="lightbox" href="{0}" data-caption="{1}" style="display: none;"></a>\n'.format(name, desc)
+            out +='            <a class="lightbox" href="{0}" data-caption="{1}" style="display: none;"></a>\n'.format(name, long_desc)
         pic_index += 1
 
     out +='<h3>{}</h3>\n'.format(desc)
